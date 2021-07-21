a784c7707d3b54dffb5abd5aed403c432a03274f
kosuha <okeydokekim@gmail.com>
2021-07-21 22:34:18 +0900

    점프 모션

--- public/jump-race/modules/Player.js
+++ public/jump-race/modules/Player.js
@@ -7,11 +7,11 @@ class Player {
         this.color = color;
         this.width = (WIDTH * width) / 400;
         this.height = (HEIGHT * height) / 700;
-        this.speed = (WIDTH * 5) / 400;;
+        this.speed = (WIDTH * 5) / 400;
         this.canvasSize = {
             x: WIDTH,
-            y: HEIGHT
-        }
+            y: HEIGHT,
+        };
         this.jumping = false;
         this.velocityX = 0;
         this.velocityY = 0;
@@ -30,14 +30,14 @@ class Player {
         return this.nickName;
     }
 
-    setPosition (positionX, positionY) {
-        this.positionX = positionX; 
+    setPosition(positionX, positionY) {
+        this.positionX = positionX;
         this.positionY = positionY;
     }
 
     display() {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.positionX, this.positionY, this.width, this.height);
+
         ctx.font = "12px san-serif";
         ctx.fillText(
             this.nickName,
@@ -45,17 +45,111 @@ class Player {
             this.positionY - (WIDTH * 10) / 400
         );
 
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.positionX + (WIDTH * 5) / 400, this.positionY + (WIDTH * 12) / 400, (WIDTH * 15) / 400, (WIDTH * 15) / 400);
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.positionX + (WIDTH * 8) / 400, this.positionY + (WIDTH * 15) / 400, (WIDTH * 8) / 400, (WIDTH * 8) / 400);
+        if (this.jumping === true) {
+            ctx.fillRect(
+                this.positionX + (WIDTH * 5) / 400,
+                this.positionY,
+                this.width - (WIDTH * 10) / 400,
+                this.height + (WIDTH * 10) / 400
+            );
 
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.positionX + (WIDTH * 30) / 400, this.positionY + (WIDTH * 12) / 400, (WIDTH * 15) / 400, (WIDTH * 15) / 400);
-        ctx.fillStyle = "black";
-        ctx.fillRect(this.positionX + (WIDTH * 33) / 400, this.positionY + (WIDTH * 15) / 400, (WIDTH * 8) / 400, (WIDTH * 8) / 400);
+            ctx.lineWidth = (WIDTH * 2) / 400;
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.moveTo(
+                this.positionX + (WIDTH * 5) / 400,
+                this.positionY + (WIDTH * 12) / 400
+            );
+            ctx.lineTo(
+                this.positionX + (WIDTH * 18) / 400,
+                this.positionY + (WIDTH * 18) / 400
+            );
+            ctx.lineTo(
+                this.positionX + (WIDTH * 5) / 400,
+                this.positionY + (WIDTH * 24) / 400
+            );
+            ctx.stroke();
 
-        
+            ctx.beginPath();
+            ctx.moveTo(
+                this.positionX + (WIDTH * (50 - 5)) / 400,
+                this.positionY + (WIDTH * 12) / 400
+            );
+            ctx.lineTo(
+                this.positionX + (WIDTH * (50 - 18)) / 400,
+                this.positionY + (WIDTH * 18) / 400
+            );
+            ctx.lineTo(
+                this.positionX + (WIDTH * (50 - 5)) / 400,
+                this.positionY + (WIDTH * 24) / 400
+            );
+            ctx.stroke();
+        } else {
+            ctx.fillRect(
+                this.positionX,
+                this.positionY,
+                this.width,
+                this.height
+            );
+
+            ctx.fillStyle = "white";
+            ctx.fillRect(
+                this.positionX + (WIDTH * 5) / 400,
+                this.positionY + (WIDTH * 12) / 400,
+                (WIDTH * 15) / 400,
+                (WIDTH * 15) / 400
+            );
+            ctx.fillRect(
+                this.positionX + (WIDTH * 30) / 400,
+                this.positionY + (WIDTH * 12) / 400,
+                (WIDTH * 15) / 400,
+                (WIDTH * 15) / 400
+            );
+
+            if (this.velocityX < 0) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 33 - 3) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 8 - 3) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+            } else if (this.velocityX > 0) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 33 + 3) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 8 + 3) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+            } else {
+                ctx.fillStyle = "black";
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 33) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+                ctx.fillRect(
+                    this.positionX + (WIDTH * 8) / 400,
+                    this.positionY + (WIDTH * 15) / 400,
+                    (WIDTH * 8) / 400,
+                    (WIDTH * 8) / 400
+                );
+            }
+        }
     }
 
     lerp(start, end, amt) {
